95a5055e1664567867cbe3e99e5cef8fca835d2e
mukuro0718 <mukuro0718@icloud.com>
2024-08-21 16:13:26 +0900

    Json,Math,STL,Vector4構造体を使えるようにするヘッダー

---
+++ Project/UseSTL.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <list>
+#include <functional>
+#include <map>
+#include <array>
+using namespace std;
---
+++ Project/Vector4.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Vector4
+{
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+	float w = 0.0f;
+};
