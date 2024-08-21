acf4ac43225381541e7294ba5ff91c5ae33f8c0b
mukuro0718 <mukuro0718@icloud.com>
2024-08-21 16:04:55 +0900

    ゲームシーンクラスの追加

---
+++ Project/GameScene.cpp
@@ -0,0 +1,138 @@
+#include <DxLib.h>
+#include "UseSTL.h"
+//#include "VECTORtoUseful.h"
+#include "GameScene.h"
+#include "SceneChanger.h"
+#include "CameraManager.h"
+#include "MapManager.h"
+#include "Debug.h"
+#include "InputManager.h"
+#include "PlayerManager.h"
+#include "EnemyManager.h"
+#include "PlayerMagicManager.h"
+#include "CollisionManager.h"
+#include "UIManager.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+GameScene::GameScene()
+	: gameState(this->TITLE)
+{
+	Initialize();
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+GameScene::~GameScene()
+{
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void GameScene::Initialize()
+{
+	/*シングルトンクラスのインスタンスを取得*/
+	auto& camera = Singleton<CameraManager>::GetInstance();
+	auto& map = Singleton<MapManager>::GetInstance();
+	auto& player = Singleton<PlayerManager>::GetInstance();
+	auto& enemy = Singleton<EnemyManager>::GetInstance();
+	auto& ui = Singleton<UIManager>::GetInstance();
+	
+	/*初期化*/
+	camera.Initialize();
+	map.Initialize();
+	enemy.Initialize();
+	player.Initialize();
+	ui.Initialize();
+}
+
+/// <summary>
+/// 最終処理
+/// </summary>
+void GameScene::Finalize()
+{
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void GameScene::Update()
+{
+	/*シングルトンクラスのインスタンスを取得*/
+	auto& input		  = Singleton<InputManager>		 ::GetInstance();
+	auto& debug		  = Singleton<Debug>			 ::GetInstance();
+	auto& camera	  = Singleton<CameraManager>	 ::GetInstance();
+	auto& map		  = Singleton<MapManager>		 ::GetInstance();
+	auto& player	  = Singleton<PlayerManager>	 ::GetInstance();
+	auto& enemy		  = Singleton<EnemyManager>		 ::GetInstance();
+	auto& playerMagic = Singleton<PlayerMagicManager>::GetInstance();
+	auto& collision   = Singleton<CollisionManager>	 ::GetInstance();
+	auto& ui = Singleton<UIManager>::GetInstance();
+
+	/*座標移動*/
+	player.Action();
+
+	/*座標の補正を行う*/
+	collision.HitCheck();
+
+	/*更新処理*/
+	input.Update();
+	debug.Update();
+	camera.Update();
+	map.Update();
+	enemy.Update();
+	player.Update();
+	playerMagic.Update();
+	ui.Update();
+
+	/*終了処理*/
+	ChangeState();
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+const void GameScene::Draw()const
+{
+	/*シングルトンクラスのインスタンスを取得*/
+	auto& debug		  = Singleton<Debug>			 ::GetInstance();
+	auto& map		  = Singleton<MapManager>		 ::GetInstance();
+	auto& player	  = Singleton<PlayerManager>	 ::GetInstance();
+	auto& camera	  = Singleton<CameraManager>	 ::GetInstance();
+	auto& enemy		  = Singleton<EnemyManager>		 ::GetInstance();
+	auto& playerMagic = Singleton<PlayerMagicManager>::GetInstance();
+	auto& collision	  = Singleton<CollisionManager>	 ::GetInstance();
+	auto& ui		  = Singleton<UIManager>::GetInstance();
+
+	/*描画*/
+	printfDx("GAME");
+	camera.Draw();
+	map.Draw();
+	debug.Draw();
+	enemy.Draw();
+	playerMagic.Draw();
+	player.Draw();
+	collision.DebugDrawHit();
+	ui.Draw();
+}
+
+/// <summary>
+/// 終了するか
+/// </summary>
+void GameScene::ChangeState()
+{
+	/*タイトル中に何かボタンが押されたら*/
+	this->gameState = this->TUTORIAL;
+
+	/*チュートリアルがすべて終了したら*/
+	this->gameState = this->GAME;
+
+	/*ボスまたはプレイヤーのHPが０になったら*/
+	this->gameState = this->RESULT;
+
+	/*リザルトの表示が終わったら*/
+	this->gameState = this->TITLE;
+}
---
+++ Project/GameScene.h
@@ -0,0 +1,31 @@
+//===========================================================================
+// @brief ゲームシーンクラス
+// ゲーム本編に必要な、各クラスのインスタンスを保持し、
+// シーンベースクラスに基づいた実装を行う
+//===========================================================================
+#pragma once
+#include "SceneBase.h"
+
+class GameScene : public SceneBase
+{
+public:
+	GameScene(); //コンストラクタ
+	~GameScene();//デストラクタ
+
+	void		Initialize() override;		//初期化
+	void		Finalize  () override;		//最終処理
+	void		Update	  () override;		//更新
+	const void	Draw	  () const override;//描画
+private:
+	/*静的定数*/
+	static constexpr unsigned int TITLE		= (1 << 0);//タイトル
+	static constexpr unsigned int TUTORIAL	= (1 << 1);//チュートリアル
+	static constexpr unsigned int GAME		= (1 << 2);//ゲーム
+	static constexpr unsigned int RESULT	= (1 << 3);//リザルト
+
+	/*内部処理関数*/
+	void ChangeState();//ゲームの状態の変更
+
+	unsigned int gameState;//ゲームの状態（本当はScene~にしたかったが、ほかの命名と被るため変更）
+};
+
