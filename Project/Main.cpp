33ffef1d1f32992a5c4397843c7b36d75a464b12
mukuro0718 <mukuro0718@icloud.com>
2024-08-21 16:07:21 +0900

    メインの追加

---
+++ Project/Main.cpp
@@ -0,0 +1,59 @@
+#include <DxLib.h>
+#include "UseSTL.h"
+#include "UseJson.h"
+#include "JsonLoader.h"
+#include "LoadingAsset.h"
+#include "ScreenSetup.h"
+#include "SceneChanger.h"
+#include "SceneManager.h"
+#include "Debug.h"
+#include "InputManager.h"
+#include "CameraManager.h"
+#include "MapManager.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // 使用する Direct3D のバージョンを設定
+    SetUseDirect3DVersion(DX_DIRECT3D_11);
+
+    /*シングルトンクラスのインスタンスを作成*/
+    //Singleton<JsonLoader>   ::CreateInstance();
+    //Singleton<JsonManager>  ::CreateInstance();
+    //Singleton<LoadingAsset> ::CreateInstance();
+    //Singleton<Debug>        ::CreateInstance();
+    //Singleton<CameraManager>::CreateInstance();
+    //Singleton<MapManager>   ::CreateInstance();
+    //Singleton<InputManager> ::CreateInstance();
+    //Singleton<ScreenSetup>  ::CreateInstance();
+    //Singleton<SceneManager> ::CreateInstance();
+    //Singleton<SceneChanger> ::CreateInstance();
+
+    /*スクリーン設定*/
+    auto& screenSetup = Singleton<ScreenSetup>::GetInstance();
+    if (screenSetup.Initialize() == -1) return -1; 
+
+    /*ゲーム本編*/
+    while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+    {
+        auto& scene = Singleton<SceneManager>::GetInstance();
+        scene.Update();
+    }
+
+    /*シングルトンクラスのインスタンスを作成*/
+    //Singleton<SceneChanger> ::DeleteInstance();
+    //Singleton<SceneManager> ::DeleteInstance();
+    //Singleton<ScreenSetup>  ::DeleteInstance();
+    //Singleton<InputManager> ::DeleteInstance();
+    //Singleton<LoadingAsset> ::DeleteInstance();
+    //Singleton<MapManager>   ::DeleteInstance();
+    //Singleton<Debug>        ::DeleteInstance();
+    //Singleton<CameraManager>::DeleteInstance();
+    //Singleton<JsonLoader>   ::DeleteInstance();
+    //Singleton<JsonManager>  ::DeleteInstance();
+
+    // ＤＸライブラリの後始末
+    DxLib_End();
+
+    // ソフトの終了
+    return 0;
+}
