cd8311b2aeec5708100b5d1fbee85425b7fe2ce2
mukuro0718 <mukuro0718@icloud.com>
2024-08-21 16:11:51 +0900

    シングルトンクラスの追加

---
+++ Project/Singleton.cpp
@@ -0,0 +1,4 @@
+#include "Singleton.h"
+
+//template <typename T>
+//T* Singleton<T>::instance = nullptr;
---
+++ Project/Singleton.h
@@ -0,0 +1,58 @@
+//===========================================================
+// @brief　シングルトンテンプレートクラス
+// 参考:https://qiita.com/narumi_/items/b205e59e7fc81695f380
+//===========================================================
+#pragma once
+template <typename T>
+class Singleton
+{
+public:
+	/*インスタンスの取得*/
+	static T& GetInstance()
+	{
+		static T instance;
+		return instance;
+	}
+protected:
+	Singleton() { }
+	virtual ~Singleton(){}
+
+private:
+	void operator=(const Singleton& _object){}//代入演算子禁止
+	Singleton(const Singleton& _object){}//コピーコンストラクタ禁止
+};
+//#pragma once
+//template <typename T>
+//class Singleton
+//{
+//public:
+//	/*インスタンスの作成*/
+//	static void CreateInstance()
+//	{
+//		instance = new T();
+//	}
+//
+//	/*インスタンスの削除*/
+//	static void DeleteInstance()
+//	{
+//		if (instance != nullptr)
+//		{
+//			delete(instance);
+//			instance = nullptr;
+//		}
+//	}
+//
+//	/*インスタンスを取得*/
+//	static T& GetInstance()
+//	{
+//		return *instance;
+//	}
+//
+//protected:
+//	/*内部処理関数*/
+//	Singleton() { }
+//	virtual ~Singleton() {}
+//	/*メンバ変数*/
+//	static T* instance;
+//};
+
