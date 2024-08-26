9d7a0158f2cc6078bb2837c7bb2c2c9ed179ab64
mukuro0718 <mukuro0718@icloud.com>
2024-08-26 09:26:08 +0900

    ボスのモデル、テクスチャ、アニメーションの追加

--- Project/Boss.h
+++ Project/Boss.h
@@ -21,17 +21,36 @@ public:
 	const Collider GetCollider();
 private:
 	/*静的定数*/
-	static constexpr int IDLE		= (1 >> 0);//待機
-	static constexpr int TAUNT		= (1 >> 1);//咆哮
-	static constexpr int WALK		= (1 >> 2);//歩き
-	static constexpr int MASK_ALL = IDLE | TAUNT | WALK;
+	static constexpr int IDLE					 = (1 >> 0);//待機
+	static constexpr int TAUNT					 = (1 >> 1);//咆哮
+	static constexpr int WALK_FRONT			 	 = (1 >> 2);//前歩き
+	static constexpr int WALK_LEFT				 = (1 >> 3);//左歩き
+	static constexpr int WALK_RIGHT				 = (1 >> 4);//右歩き
+	static constexpr int VERTICAL_SLASH			 = (1 >> 5);//縦切り
+	static constexpr int HORIZONTAL_SLASH		 = (1 >> 6);//横切り
+	static constexpr int ROTATION_SLASH			 = (1 >> 7);//回転切り
+	static constexpr int KNOCK_UP_SLASH			 = (1 >> 8);//下から上切り
+	static constexpr int STRONG_HORIZONTAL_SLASH = (1 >> 9);//強い横切り
+	static constexpr int TWO_COMBO				 = (1 >> 10);//１２コンボ
+	static constexpr int STRONG_TWO_COMBO		 = (1 >> 11);//強い１２コンボ横切り
+	static constexpr int THREE_COMBO			 = (1 >> 12);//１２３コンボ
 
 	/*列挙体*/
 	enum class AnimationType
 	{
-		RUSH = 0,
-		TAUNT = 1,
-		WALK = 2,
+		IDLE					= 0,//待機
+		TAUNT					= 1,//咆哮
+		WALK_FRONT				= 2,//前歩き
+		WALK_LEFT				= 3,//左歩き
+		WALK_RIGHT				= 4,//右歩き
+		VERTICAL_SLASH			= 5,//縦切り
+		HORIZONTAL_SLASH		= 6,//横切り
+		ROTATION_SLASH			= 7,//回転切り
+		KNOCK_UP_SLASH			= 8,//下から上切り
+		STRONG_HORIZONTAL_SLASH = 9,//強い横切り
+		TWO_COMBO				= 10,//１２コンボ
+		STRONG_TWO_COMBO		= 11,//強い１２コンボ横切り
+		THREE_COMBO				= 12,//１２３コンボ
 	};
 
 	/*内部処理関数*/
