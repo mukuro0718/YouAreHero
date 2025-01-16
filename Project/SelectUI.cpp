c522ef29f671336fc34a59970d9a2549a690bd25
mukuro0718 <mukuro0718@icloud.com>
2025-01-16 09:56:27 +0900

    ボス選択画面のUIを一新

--- Project/SelectUI.cpp
+++ Project/SelectUI.cpp
@@ -17,30 +17,31 @@
 /// </summary>
 SelectUI::SelectUI()
 	: background	(-1)
-	, logo			(-1)
-	, iconFrame		(-1)
+	, iconTable		(-1)
+	, frame			(-1)
 	, provDecide	(-1)
 	, nowSelectEnemy(0)
 	, isEnd			(false)
 {
-	/*画像ハンドルの取得*/
+	/*アセットの取得*/
 	auto& asset = Singleton<LoadingAsset>::GetInstance();
-	this->background = asset.GetImage(LoadingAsset::ImageType::SELECT_BACK);
-	this->logo		 = asset.GetImage(LoadingAsset::ImageType::SELECT_LOGO);
-	this->provDecide = asset.GetImage(LoadingAsset::ImageType::SELECT_PROV_DECIDE);
-	this->iconFrame  = asset.GetImage(LoadingAsset::ImageType::SELECT_ICON_FRAME);
-	this->icon.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_ICON_1));
-	this->icon.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_ICON_2));
-	this->icon.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_ICON_3));
-	this->icon.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_ICON_4));
-	this->image.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_1));
-	this->image.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_2));
-	this->image.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_3));
-	this->image.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_4));
-	this->summary.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_SUMMARY_1));
-	this->summary.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_SUMMARY_2));
-	this->summary.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_SUMMARY_3));
-	this->summary.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_SUMMARY_4));
+	this->headerFont		 = asset.GetFont(LoadingAsset::FontType::SELECT_HEADER);
+	this->questFont			 = asset.GetFont(LoadingAsset::FontType::SELECT_QUEST);
+	this->actionFont		 = asset.GetFont(LoadingAsset::FontType::SELECT_ACTION);
+	this->background		 = asset.GetImage(LoadingAsset::ImageType::SELECT_BACK);
+	this->provDecide		 = asset.GetImage(LoadingAsset::ImageType::SELECT_PROV_DECIDE);
+	this->frame				 = asset.GetImage(LoadingAsset::ImageType::SELECT_FRAME);
+	this->iconTable			 = asset.GetImage(LoadingAsset::ImageType::SELECT_ICON_TABLE);
+	this->enemyImageTable	 = asset.GetImage(LoadingAsset::ImageType::SELECT_IMAGE_TABLE);
+	this->drawRectBackGround = asset.GetImage(LoadingAsset::ImageType::SELECT_DRAW_RECT);
+	this->aButton = asset.GetImage(LoadingAsset::ImageType::A_BUTTON);
+	this->bButton = asset.GetImage(LoadingAsset::ImageType::B_BUTTON);
+	this->lStick = asset.GetImage(LoadingAsset::ImageType::LEFT_STICK_INPUT);
+	this->enemyImage.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_1));
+	this->enemyImage.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_2));
+	this->enemyImage.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_3));
+	this->enemyImage.emplace_back(asset.GetImage(LoadingAsset::ImageType::QUEST_IMAGE_4));
+
 }
 
 /// <summary>
@@ -48,9 +49,7 @@ SelectUI::SelectUI()
 /// </summary>
 SelectUI::~SelectUI()
 {
-	this->icon.clear();
-	this->image.clear();
-	this->summary.clear();
+	this->enemyImage.clear();
 }
 
 /// <summary>
@@ -84,45 +83,97 @@ void SelectUI::Update()
 const void SelectUI::Draw()const
 {
 	auto& json = Singleton<JsonManager>::GetInstance();
-
+	vector<int> fontColorIndex = json.GetJson(JsonManager::FileType::UI)["SELECT_FONT_COLOR"];
+	int fontColor = GetColor(fontColorIndex[0], fontColorIndex[1], fontColorIndex[2]);
 	/*背景*/
 	{
 		vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_BACKGROUND_POSITION"];
-		DrawExtendGraph(position[0], position[1], position[2], position[3], this->background, TRUE);
+		vector<int> headerPosition = json.GetJson(JsonManager::FileType::UI)["SELECT_MENU_HEADER_POSITION"];
+		vector<int> headerLinePosition = json.GetJson(JsonManager::FileType::UI)["SELECT_MENU_HEADER_LINE_POSITION"];
+		vector<int> colorIndex = json.GetJson(JsonManager::FileType::UI)["SELECT_BACKGROUND_COLOR"];
+		int color = GetColor(colorIndex[0], colorIndex[1], colorIndex[2]);
+		DrawBox(position[0], position[1], position[2], position[3], color, TRUE);
+		DrawStringToHandle(headerPosition[0], headerPosition[1], "選択画面", fontColor, this->headerFont);
+		DrawLine(headerLinePosition[0], headerLinePosition[1], headerLinePosition[2], headerLinePosition[3], fontColor);
+
+	}
+
+
+	/*描画範囲*/
+	{
+		vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_DRAW_RECT_POSITION"];
+		DrawExtendGraph(position[0], position[1], position[2], position[3], this->drawRectBackGround, TRUE);//全体の枠
 	}
 
 	/*イメージ画像*/
 	{
 		vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_IMAGE_POSITION"];
-		DrawExtendGraph(position[0], position[1], position[2], position[3], this->image[this->nowSelectEnemy], TRUE);
+		DrawExtendGraph(position[0], position[1], position[2], position[3], this->enemyImageTable, TRUE);
+		DrawExtendGraph(position[0], position[1], position[2], position[3], this->enemyImage[this->nowSelectEnemy], TRUE);
 	}
 
 	/*ロゴ*/
 	{
-		vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_LOGO_POSITION"];
-		DrawExtendGraph(position[0], position[1], position[2], position[3], this->logo, TRUE);
+		//vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_LOGO_POSITION"];
+		
 	}
 
 	/*アイコン*/
 	{
-		vector<vector<int>> position = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_POSITION"];
-		for (int i = 0; i < position.size(); i++)
-		{
-			DrawExtendGraph(position[i][0], position[i][1], position[i][2], position[i][3], this->icon[i], TRUE);
-		}
+		int iconNum = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_NUM"];
+		vector<int> positionOffset = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_TEXT_OFFSET"];
+		vector<int> position1 = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_POSITION"][0];
+		vector<int> position2 = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_POSITION"][1];
+		vector<int> position3 = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_POSITION"][2];
+		vector<int> position4 = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_POSITION"][3];
+		vector<int> headerPosition = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_HEADER_POSITION"];
+		vector<int> headerLinePosition = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_HEADER_LINE_POSITION"];
+
+		DrawStringToHandle(headerPosition[0], headerPosition[1], "クエスト一覧", fontColor, this->headerFont);
+		DrawLine(headerLinePosition[0], headerLinePosition[1], headerLinePosition[2], headerLinePosition[3], fontColor);
+		DrawExtendGraph(position1[0], position1[1], position1[2], position1[3], this->iconTable, TRUE);
+		DrawStringToHandle(position1[0] + positionOffset[0], position1[1] + positionOffset[1], "討伐：GORG GRASS", fontColor, this->questFont);
+		DrawExtendGraph(position2[0], position2[1], position2[2], position2[3], this->iconTable, TRUE);
+		DrawStringToHandle(position2[0] + positionOffset[0], position2[1] + positionOffset[1], "討伐：LUXURIO", fontColor, this->questFont);
+		DrawExtendGraph(position3[0], position3[1], position3[2], position3[3], this->iconTable, TRUE);
+		DrawStringToHandle(position3[0] + positionOffset[0], position3[1] + positionOffset[1], "討伐：MORNACT", fontColor, this->questFont);
+		DrawExtendGraph(position4[0], position4[1], position4[2], position4[3], this->iconTable, TRUE);
+		DrawStringToHandle(position4[0] + positionOffset[0], position4[1] + positionOffset[1], "チュートリアル", fontColor, this->questFont);
 	}
 
 	/*フレーム*/
 	{
 		vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_ICON_POSITION"][this->nowSelectEnemy];
-		DrawExtendGraph(position[0], position[1], position[2], position[3], this->iconFrame, TRUE);
+		int frameOffset = json.GetJson(JsonManager::FileType::UI)["SELECT_FRAME_POSITION_OFFSET"];
+		DrawExtendGraph(position[0] - frameOffset, position[1] - frameOffset, position[2] + frameOffset, position[3] + frameOffset, this->frame, TRUE);
 	}
 
-	/*サマリー*/
+	/*アクション*/
 	{
-		vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_SUMMARY_POSITION"];
-		DrawExtendGraph(position[0], position[1], position[2], position[3], this->summary[this->nowSelectEnemy], TRUE);
+		vector<int> decidePosition	= json.GetJson(JsonManager::FileType::UI)["SELECT_DECIDE_TEXT_POSITION"];
+		vector<int> decideButtonPosition	= json.GetJson(JsonManager::FileType::UI)["SELECT_DECIDE_BUTTON_POSITION"];
+		vector<int> backPosition	= json.GetJson(JsonManager::FileType::UI)["SELECT_BACK_TEXT_POSITION"];
+		vector<int> backButtonPosition	= json.GetJson(JsonManager::FileType::UI)["SELECT_BACK_BUTTON_POSITION"];
+		vector<int> selectPosition	= json.GetJson(JsonManager::FileType::UI)["SELECT_SELECT_TEXT_POSITION"];
+		vector<int> selectButtonPosition	= json.GetJson(JsonManager::FileType::UI)["SELECT_SELECT_BUTTON_POSITION"];
+		DrawStringToHandle(decidePosition[0], decidePosition[1], "決定", fontColor, this->actionFont);
+		DrawExtendGraph(decideButtonPosition[0], decideButtonPosition[1], decideButtonPosition[2], decideButtonPosition[3], this->bButton, TRUE);
+		DrawStringToHandle(backPosition[0], backPosition[1], "戻る", fontColor, this->actionFont);
+		DrawExtendGraph(backButtonPosition[0], backButtonPosition[1], backButtonPosition[2], backButtonPosition[3], this->aButton, TRUE);
+		DrawStringToHandle(selectPosition[0], selectPosition[1], "選択", fontColor, this->actionFont);
+		DrawExtendGraph(selectButtonPosition[0], selectButtonPosition[1], selectButtonPosition[2], selectButtonPosition[3], this->lStick, TRUE);
+
+		//vector<int> position = json.GetJson(JsonManager::FileType::UI)["SELECT_SUMMARY_POSITION"];
+		//DrawExtendGraph(position[0], position[1], position[2], position[3], this->summary[this->nowSelectEnemy], TRUE);
 	}
+
+	//DrawBox(100 + 1, 150 + 1, 1820 - 1, 980 - 1, GetColor(255, 0, 0), FALSE);//全体の枠
+	//DrawBox(760 + 1, 50 + 1, 1160 - 1, 100 - 1, GetColor(255, 0, 0), FALSE);//画面の説明
+	//DrawBox(960 + 1, 50 + 1, 960 - 1, 100 - 1, GetColor(255, 0, 0), FALSE);//画面の説明
+	//DrawBox(250 + 1, 300 + 1, 885 - 1, 700 - 1, GetColor(255, 0, 0), FALSE);//アイコンの位置y:75+25
+	//DrawBox(400 + 1, 200 + 1, 735 - 1, 250 - 1, GetColor(255, 0, 0), FALSE);//アイコンの説明
+	//DrawBox(910 + 1, 250 + 1, 1820 - 1, 880 - 1, GetColor(255, 0, 0), FALSE);//写真の位置
+	
 
 	/*最終決定*/
 	{
--- Project/SelectUI.h
+++ Project/SelectUI.h
@@ -16,15 +16,23 @@ public:
 	const void Draw		 ()const override;
 	const bool IsEnd	 ()const override;
 private:
+	/*静的定数*/
+
 	/*メンバ変数*/
-	vector<int>	icon;			//画像ハンドル
-	vector<int>	summary;		//画像ハンドル
-	vector<int> image;			//画像ハンドル
+	vector<int> enemyImage;		//敵画像
+	int			iconTable;		//アイコンテーブル
 	int			background;		//背景
-	int			logo;			//ロゴ
-	int			iconFrame;		//アイコンのフレーム
+	int			frame;			//アイコンのフレーム
 	int			provDecide;		//決定アイコン
 	int			nowSelectEnemy;	//現在選択されている敵
 	bool		isEnd;			//終了するか
+	int			headerFont;		//ヘッダーフォント
+	int			questFont;		//クエストフォント
+	int			actionFont;		//アクションフォント
+	int			enemyImageTable;//敵画像テーブル
+	int			drawRectBackGround;//描画範囲背景
+	int aButton;
+	int bButton;
+	int lStick;
 };
 
